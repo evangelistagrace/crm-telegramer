26577c71fafe7589e8734af573a3ed9e827c92d6
Grace <evangrace98@gmail.com>
2022-07-26 23:20:18 +0800

    commit

--- functions/telegramer/telegramer.js
+++ functions/telegramer/telegramer.js
@@ -6,42 +6,19 @@ const url = 'https://api.telegram.org/bot';
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    console.log("message: ", event.body)
-    /*
-      console.log(event.body.message)
-      console.log('success')
-      slimbot.sendMessage(process.env.TELEGRAM_CHAT_ID, 'Someone just starred your repo');
-*/
-    let {case_id, case_category, case_type} = event.body
+    let {case_id, case_title, case_category, case_type, case_type_detail} = JSON.parse(event.body)
 
     const res = await axios.post(`${url}${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,{
         chat_id: process.env.TELEGRAM_CHAT_ID,
-        text: `Case#${case_id}: \nCategory: ${case_category}`
-    })
-    .then((response) => {
+        text: `[NEW CASE]\nCase#${case_id}: ${case_title}\nType: ${case_type}\nCategory: ${case_category} [${case_type_detail}]`
+
+    }).then((response) => {
       let date = new Date()
         console.log("process executed at ", date) 
     })
-    /*
-    const body = JSON.parse(event.body);
-    const username = body.sender.login;
-    const avatarUrl = body.sender.avatar_url;
-    const repoName = body.repository.name;
-    const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    });
-    console.log("Submitted!");
-    */
     return {
       statusCode: 204,
-      statusText: "OK"
+      body: JSON.stringify({ message: "OK" }),
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
